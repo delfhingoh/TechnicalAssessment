af2a13ba765b6a7206c8ea93fed5fa55d39db3f1
delfhingoh <delfhingoh98@gmail.com>
2020-12-26 01:11:31 +0800

    Add Firestore to production part

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,17 @@
 export const environment = 
 {
   production: true,
+  // Adding Firebase
+  firebase : 
+  {
+    apiKey: "AIzaSyAtgkloQ_VZh4Fuwv_e6Ea22JXtyn92jNw",
+    authDomain: "technicalassessment-59832.firebaseapp.com",
+    projectId: "technicalassessment-59832",
+    storageBucket: "technicalassessment-59832.appspot.com",
+    messagingSenderId: "299198743263",
+    appId: "1:299198743263:web:3179beb4eb89ac5fca9f6a",
+    measurementId: "G-LZELV12ZQK"
+  }
 };
+
+
